6b64b08725ae5a97fdc411aa1d4dbb030cd00321
Juho Heinisuo <juho.heinisuo@reaktor.fi>
2016-11-14 19:58:45 +0200

    Use cache for files

--- server/files.js
+++ server/files.js
@@ -2,7 +2,8 @@ import Bluebird  from 'bluebird'
 import R         from 'ramda'
 import {resolve} from 'path'
 
-import {normalize} from '../src/js/util'
+import {normalize}        from '../src/js/util'
+import {cache, bustCache} from './cache'
 
 const fs          = Bluebird.promisifyAll(require('fs'))
 const {execAsync} = Bluebird.promisifyAll(require('child_process'))
@@ -10,17 +11,20 @@ const {execAsync} = Bluebird.promisifyAll(require('child_process'))
 const DATA_DIR = resolve(`${__dirname}/../data`)
 
 
-const getCourses = () => (
-  fs.readFileAsync(`${DATA_DIR}/_courses.json`)
+const getCourses = req => cache(
+  req.path,
+  () => fs.readFileAsync(`${DATA_DIR}/_courses.json`, 'utf8')
     .then(data => JSON.parse(data))
 )
 
-const commitAndPush = () => execAsync(
-  'git commit -am "[update course data]" && git push'
+const getCourse = req => cache(
+  req,
+  () => fs.readFileAsync(`${DATA_DIR}/${req.params.course}.md`, 'utf8')
 )
 
-const getCourseHistory = course => (
-  execAsync(`git log -- ${getFilename(course)}`)
+const getCourseHistory = req => cache(
+  req.path,
+  () => execAsync(`git log -- ${getFilename(req.params.course)}`)
     .then(R.pipe(
       R.split('\n'),
       R.filter(line => line.startsWith('commit') || line.startsWith('Date')),
@@ -30,12 +34,18 @@ const getCourseHistory = course => (
     ))
 )
 
-const getCourseAt = (course, commit) => (
-  execAsync(`git show ${commit}:${getFilename(course)}`)
+const getCourseAt = req => cache(
+  req.path,
+  () => execAsync(`git show ${req.query.commit}:${getFilename(req.params.course)}`)
+)
+
+const commitAndPush = () => execAsync(
+  'git commit -am "[update course data]" && git push'
 )
 
 export default {
   getCourses,
+  getCourse,
   getCourseHistory,
   getCourseAt,
   commitAndPush
