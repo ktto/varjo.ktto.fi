985f8b1aaa8cacb1d5c36f423eeb98f1d1edd128
Juho Heinisuo <juho.heinisuo@iki.fi>
2017-02-20 22:36:19 +0200

    Remove startup script

--- bin/start-prod.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-sudo mkdir -p /usr/local/etc/nginx
-sudo cp proxy/nginx.conf /usr/local/etc/nginx/nginx.conf
-npm run build:prod
-npm run stop:prod
-npm run start:prod
-sudo service nginx restart
-
