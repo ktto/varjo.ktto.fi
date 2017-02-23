58ade298a49ee5487e2f5fbc0b18a77ff98410a0
ktto <kttory@gmail.com>
2017-02-23 14:44:17 +0000

    Use https on share

--- server/api.js
+++ server/api.js
@@ -210,13 +210,13 @@ function renderApp (appHTML, appState, course) {
     <meta name="application-name" content="Varjo-opinto-opas" />
     <meta property="og:title" content="${title}" />
     <meta property="og:type" content="article" />
-    <meta property="og:image" content="http://varjo.ktto.fi/public/img/ktto.jpg" />
-    <meta property="og:url" content="http://varjo.ktto.fi${appState.path}" />
+    <meta property="og:image" content="https://varjo.ktto.fi/public/img/ktto.jpg" />
+    <meta property="og:url" content="https://varjo.ktto.fi${appState.path}" />
     <meta property="og:description" content="${desc}" />
     <meta name="twitter:card" content="KTTO" />
     <meta name="twitter:title" content="${title}" />
     <meta name="twitter:description" content="${desc}" />
-    <meta name="twitter:image" content="http://varjo.ktto.fi/public/img/ktto.jpg" />
+    <meta name="twitter:image" content="https://varjo.ktto.fi/public/img/ktto.jpg" />
     <link rel="stylesheet" type="text/css" href="/public/css/all.min.css">
     <title>KTTO | ${course.title ? course.title + ' | ' : ''}varjo-opinto-opas</title>
   </head>
